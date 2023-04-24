3882b007f4b3ca0f850e4175425c439be1bb27ff
kuznetsb <kuznetsbog@gmail.com>
2023-04-24 17:44:44 +0300

    api removed from token urls

--- user/urls.py
+++ user/urls.py
@@ -8,8 +8,8 @@ from user.views import UserCreateView, ManageUserView
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create_user"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 
