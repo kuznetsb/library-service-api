d9678ec18e78ffa883bc1e4c325c00ea83c31326
name <ksyusha0kubay@gmail.com>
2023-04-24 17:26:08 +0300

    Change User

--- user/models.py
+++ user/models.py
@@ -47,3 +47,18 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="user_groups",  # new related name
+        blank=True,
+        verbose_name="groups",
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+    )
+
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="user_permissions_set",  # new related name
+        blank=True,
+        verbose_name="user permissions",
+        help_text="Specific permissions for this user.",
+    )
