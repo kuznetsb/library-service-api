657d4e31bd7646b34b1fa1247d182aba4e893196
name <ksyusha0kubay@gmail.com>
2023-04-24 18:44:40 +0300

    Change permissions

--- book/views.py
+++ book/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from book.models import Book
+from book.permissions import IsAdminOrReadOnly
 from book.serializers import (
     BookSerializer,
     BookListSerializer,
@@ -12,14 +15,27 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminOrReadOnly]
+        return [permission() for permission in permission_classes]
+
     def get_serializer_class(self):
         if self.action == "list":
             return BookListSerializer
         if (
-                self.action == "retrieve" or
-                self.action == "partial_update" or
-                self.action == "update"
+            self.action == "retrieve"
+            or self.action == "partial_update"
+            or self.action == "update"
+            or self.action == "destroy"
         ):
             return BookDetailSerializer
 
         return BookSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
