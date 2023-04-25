f6ce0e43e51a47b4eb195897077bf70bdfddb92e
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-04-25 11:31:26 +0300

    fix serializers and urls in payment

--- payment/serializer.py
+++ payment/serializer.py
@@ -6,8 +6,16 @@ from payment.models import Payment
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ["id", "status", "type", "borrowing", "session_url", "session_id", "money_to_pay", "created_at"]
-        read_only_fields = ["id", "created_at"]
+        fields = [
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session",
+            "money_to_pay",
+            "created_at"
+        ]
 
 
 class PaymentListSerializer(serializers.ModelSerializer):
@@ -34,4 +42,9 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
             "payment_method",
             "session",
             "status",
+        ]
+    read_only_fields = [
+        "borrowing__book__title",
+        "borrowing__borrow_date",
+        "borrowing__user__email",
         ]
--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,5 @@ app_name = "payment"
 
 urlpatterns = [
     path("success/", PaymentListAPIView.as_view(), name="payment-list"),
-    path("success/<pk>/", PaymentDetailAPIView.as_view(), name="payment-detail"),
     path("cancel/", PaymentListAPIView.as_view(), name="payment-cancel"),
     ]
